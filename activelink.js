7b2b8b4b75d162ebc1a095d42c14c43bbb243978
Georg Puntigam <darkgeorok@gmail.com>
2021-10-09 17:46:27 +0200

    click functions

--- activelink.js
+++ activelink.js
@@ -9,21 +9,29 @@ $(function() {
 });
 
 $(function() {
-    $('#Into_the_void').click(function() {
+    $('.scorebutton').click(function() {
       // this removes the underline class from all other ".navigation" links.
+      $('.scorebutton').removeClass('currentlyActive');
+  
+      // this makes the one that was clicked underlined
+      $(this).addClass('currentlyActive');
+    });
+  });
+
+
+$(function() {
+    $('#Into_the_void').click(function() {
       displaypdf('scores/Into_the_void.pdf')
     });
   });
 
   $(function() {
     $('#In_Jubilo').click(function() {
-      // this removes the underline class from all other ".navigation" links.
       displaypdf('scores/In_Jubilo.pdf')
     });
   });
   $(function() {
     $('#Weihnachtstanz').click(function() {
-      // this removes the underline class from all other ".navigation" links.
       displaypdf('scores/Weihnachtstanz.pdf')
     });
   });
