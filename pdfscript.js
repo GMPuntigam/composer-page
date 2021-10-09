169709c3ce198c298ada901a90040fd84fec9151
Georg Puntigam <darkgeorok@gmail.com>
2021-10-09 13:14:10 +0200

    seperate js

---
+++ pdfscript.js
@@ -0,0 +1,32 @@
+function displaypdf() {
+    var myState = {
+        pdf: null,
+        currentPage: 1,
+        zoom: 1
+    }
+
+    pdfjsLib.getDocument('Into_the_void.pdf').then((pdf) => {
+
+        myState.pdf = pdf;
+        render();
+
+    });
+
+    function render() {
+        myState.pdf.getPage(myState.currentPage).then((page) => {
+        
+            var canvas = document.getElementById("pdf_renderer");
+            var ctx = canvas.getContext('2d');
+
+            var viewport = page.getViewport(myState.zoom);
+
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+        
+            page.render({
+                canvasContext: ctx,
+                viewport: viewport
+            });
+        });
+    }
+}
