06758fa7da7658057b47a3b57c11b2de56e9c329
Georg Puntigam <darkgeorok@gmail.com>
2021-10-10 13:02:55 +0200

    audioplayer

--- audioplayer.js
+++ audioplayer.js
@@ -37,9 +37,12 @@ playAnimation.goToAndStop(14, true);
 for (let playIconContainer of playIconContainers){
     playIconContainer.addEventListener('click', function(event) {
         var targetElement = event.target;
-        var elementID = targetElement.parentElement.id;
         if (event.target == '') {
             var targetElement = event.currentTarget
+            var elementID = event.currentTarget.parentNode.children[1].id
+        }
+        var elementID = targetElement.parentElement.id;
+        if (elementID == '') {
             var elementID = event.currentTarget.parentNode.children[1].id
         }
         var animationID = elementID[elementID["length"]-1]
