08411260239f8f1caad176e6a9ea9f59a585112e
Georg Puntigam <darkgeorok@gmail.com>
2021-10-10 10:13:14 +0200

    audioplayer

--- audioplayer.js
+++ audioplayer.js
@@ -1,9 +1,11 @@
-/** Implementation of the presentation of the audio player */
+/* Implementation of the presentation of the audio player */
 import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
 
 const playIconContainer = document.getElementById('play-icon');
 const audioPlayerContainer = document.getElementById('audio-player-container');
 const seekSlider = document.getElementById('seek-slider');
+const volumeSlider = document.getElementById('volume-slider');
+const muteIconContainer = document.getElementById('mute-icon');
 let playState = 'play';
 let muteState = 'unmute';
 
@@ -14,6 +16,15 @@ const playAnimation = lottieWeb.loadAnimation({
   loop: false,
   autoplay: false,
   name: "Play Animation",
+});
+
+const muteAnimation = lottieWeb.loadAnimation({
+    container: muteIconContainer,
+    path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/mute/mute.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    name: "Mute Animation",
 });
 
 playAnimation.goToAndStop(14, true);
@@ -32,19 +43,40 @@ playIconContainer.addEventListener('click', () => {
     }
 });
 
+muteIconContainer.addEventListener('click', () => {
+    if(muteState === 'unmute') {
+        muteAnimation.playSegments([0, 15], true);
+        audio.muted = true;
+        muteState = 'mute';
+    } else {
+        muteAnimation.playSegments([15, 25], true);
+        audio.muted = false;
+        muteState = 'unmute';
+    }
+});
+
 const showRangeProgress = (rangeInput) => {
     if(rangeInput === seekSlider) audioPlayerContainer.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+    else audioPlayerContainer.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
 }
 
 seekSlider.addEventListener('input', (e) => {
     showRangeProgress(e.target);
 });
+volumeSlider.addEventListener('input', (e) => {
+    showRangeProgress(e.target);
+});
 
-/** Implementation of the functionality of the audio player */
+
+
+
+
+/* Implementation of the functionality of the audio player */
 
 const audio = document.querySelector('audio');
 const durationContainer = document.getElementById('duration');
 const currentTimeContainer = document.getElementById('current-time');
+const outputContainer = document.getElementById('volume-output');
 let raf = null;
 
 const calculateTime = (secs) => {
@@ -101,3 +133,80 @@ seekSlider.addEventListener('change', () => {
         requestAnimationFrame(whilePlaying);
     }
 });
+
+volumeSlider.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    outputContainer.textContent = value;
+    audio.volume = value / 100;
+});
+
+
+
+
+/* Implementation of the Media Session API */
+if('mediaSession' in navigator) {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'Komorebi',
+        artist: 'Anitek',
+        album: 'MainStay',
+        artwork: [
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '96x96', type: 'image/png' },
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '128x128', type: 'image/png' },
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '192x192', type: 'image/png' },
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '256x256', type: 'image/png' },
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '384x384', type: 'image/png' },
+            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '512x512', type: 'image/png' }
+        ]
+    });
+    navigator.mediaSession.setActionHandler('play', () => {
+        if(playState === 'play') {
+            audio.play();
+            playAnimation.playSegments([14, 27], true);
+            requestAnimationFrame(whilePlaying);
+            playState = 'pause';
+        } else {
+            audio.pause();
+            playAnimation.playSegments([0, 14], true);
+            cancelAnimationFrame(raf);
+            playState = 'play';
+        }
+    });
+    navigator.mediaSession.setActionHandler('pause', () => {
+        if(playState === 'play') {
+            audio.play();
+            playAnimation.playSegments([14, 27], true);
+            requestAnimationFrame(whilePlaying);
+            playState = 'pause';
+        } else {
+            audio.pause();
+            playAnimation.playSegments([0, 14], true);
+            cancelAnimationFrame(raf);
+            playState = 'play';
+        }
+    });
+    navigator.mediaSession.setActionHandler('seekbackward', (details) => {
+        audio.currentTime = audio.currentTime - (details.seekOffset || 10);
+    });
+    navigator.mediaSession.setActionHandler('seekforward', (details) => {
+        audio.currentTime = audio.currentTime + (details.seekOffset || 10);
+    });
+    navigator.mediaSession.setActionHandler('seekto', (details) => {
+        if (details.fastSeek && 'fastSeek' in audio) {
+          audio.fastSeek(details.seekTime);
+          return;
+        }
+        audio.currentTime = details.seekTime;
+    });
+    navigator.mediaSession.setActionHandler('stop', () => {
+        audio.currentTime = 0;
+        seekSlider.value = 0;
+        audioPlayerContainer.style.setProperty('--seek-before-width', '0%');
+        currentTimeContainer.textContent = '0:00';
+        if(playState === 'pause') {
+            playAnimation.playSegments([0, 14], true);
+            cancelAnimationFrame(raf);
+            playState = 'play';
+        }
+    });
+}
