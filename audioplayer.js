a83592b591e645a3322aee6f288a7dfcfd1f1613
Georg Puntigam <darkgeorok@gmail.com>
2021-10-10 14:08:25 +0200

    audio pause when completed

--- audioplayer.js
+++ audioplayer.js
@@ -124,6 +124,10 @@ const whilePlaying = () => {
     currentTimeContainer.textContent = calculateTime(seekSlider.value);
     audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);
     raf = requestAnimationFrame(whilePlaying);
+    if (audio.currentTime == floor(audio.duration)) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
 }
 
 if (audio.readyState > 0) {
