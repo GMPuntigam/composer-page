a6cc8bec23906fbaf433c938748ef160bf8529e2
Georg Puntigam <darkgeorok@gmail.com>
2021-10-11 11:44:20 +0200

    remove jquery

--- activelink.js
+++ activelink.js
@@ -1,37 +1,56 @@
-$(function() {
-  $('.navigation').click(function() {
-    // this removes the underline class from all other ".navigation" links.
-    $('.navigation').removeClass('currentlyActive');
-
-    // this makes the one that was clicked underlined
-    $(this).addClass('currentlyActive');
+document.addEventListener("DOMContentLoaded", function () {
+  const NavigationClasses = [".navigation", ".scorelink"];
+  NavigationClasses.forEach(function (item, index) {
+    for (let navigationlink of document.querySelectorAll(item)) {
+      navigationlink.addEventListener('click', function (e) {
+        e = e || window.event;
+        var target = e.currentTarget;
+        for (let navigationlink of document.querySelectorAll(item)) {
+          navigationlink.classList.remove("currentlyActive");
+        }
+        target.classList.add('currentlyActive')
+      });
+    }
   });
 });
 
-$(function() {
-    $('.scorebutton').click(function() {
-      // this removes the underline class from all other ".navigation" links.
-      $('.scorebutton').removeClass('currentlyActive');
-  
-      // this makes the one that was clicked underlined
-      $(this).addClass('currentlyActive');
-    });
-  });
+
+// for (let navigationlink of document.getElementsByClassName('navigation')) {
+//   navigationlink.addEventListener('click', function (e) {
+//     e = e || window.event;
+//     var target = e.target;
+//     for (let navigationlink of document.getElementsByClassName('navigation')) {
+//       navigationlink.classList.remove("currentlyActive");
+//     }
+//     target.classList.add('currentlyActive')
+//   });
+// }
 
 
-$(function() {
-    $('#Into_the_void').click(function() {
-      displaypdf('scores/Into_the_void.pdf')
-    });
-  });
+// $(function () {
+//   $('.scorebutton').click(function () {
+//     // this removes the underline class from all other ".navigation" links.
+//     $('.scorebutton').removeClass('currentlyActive');
 
-  $(function() {
-    $('#In_Jubilo').click(function() {
-      displaypdf('scores/In_Jubilo.pdf')
-    });
+//     // this makes the one that was clicked underlined
+//     $(this).addClass('currentlyActive');
+//   });
+// });
+
+
+$(function () {
+  $('#Into_the_void').click(function () {
+    displaypdf('scores/Into_the_void.pdf')
   });
-  $(function() {
-    $('#Weihnachtstanz').click(function() {
-      displaypdf('scores/Weihnachtstanz.pdf')
-    });
+});
+
+$(function () {
+  $('#In_Jubilo').click(function () {
+    displaypdf('scores/In_Jubilo.pdf')
   });
+});
+$(function () {
+  $('#Weihnachtstanz').click(function () {
+    displaypdf('scores/Weihnachtstanz.pdf')
+  });
+});
