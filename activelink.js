78e58049a6c688cf3fc90a4e04f805ef7caef49a
Georg Puntigam <darkgeorok@gmail.com>
2021-10-11 12:46:44 +0200

    improve layout

--- activelink.js
+++ activelink.js
@@ -1,3 +1,5 @@
+var pdftarget = "Into_the_void.pdf"
+
 document.addEventListener("DOMContentLoaded", function () {
   const NavigationClasses = [".navigation", ".scorelink"];
   NavigationClasses.forEach(function (item, index) {
@@ -8,7 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
         for (let navigationlink of document.querySelectorAll(item)) {
           navigationlink.classList.remove("currentlyActive");
         }
-        target.classList.add('currentlyActive')
+        target.classList.add('currentlyActive');
+        currentstring = e.currentTarget.innerText.replaceAll(' ', '_') + '.pdf';
+        if (currentstring != pdftarget && item == '.scorelink') {
+          pdftarget = currentstring;
+          displaypdf('scores/' + pdftarget);
+        }
 
       });
     }
@@ -37,17 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
       displaypdf('scores/Weihnachtstanz.pdf')
     }
   });
-
-  document.getElementById('Into_the_void').addEventListener('click', function (e) {
-    displaypdf('scores/Into_the_void.pdf')
-  });
-
-  document.getElementById('In_Jubilo').addEventListener('click', function (e) {
-    displaypdf('scores/In_Jubilo.pdf')
-  });
-
-  document.getElementById('Weihnachtstanz').addEventListener('click', function (e) {
-    displaypdf('scores/Weihnachtstanz.pdf')
-  });
 });
+//   document.getElementById('Into_the_void').addEventListener('click', function (e) {
+//     displaypdf('scores/Into_the_void.pdf')
+//   });
+
+//   document.getElementById('In_Jubilo').addEventListener('click', function (e) {
+//     displaypdf('scores/In_Jubilo.pdf')
+//   });
+
+//   document.getElementById('Weihnachtstanz').addEventListener('click', function (e) {
+//     displaypdf('scores/Weihnachtstanz.pdf')
+//   });
+// 
 
