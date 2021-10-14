31f99a11fafaa3cbe21de62ce731e91c2835a6f5
Georg Puntigam <darkgeorok@gmail.com>
2021-10-14 10:31:23 +0200

    improve layouts

--- pdfscript.js
+++ pdfscript.js
@@ -3,30 +3,44 @@ var myState = {
     currentPage: 1,
     zoom: 0.65
 }
-var orientation_dummy = "default"
-screen.orientation.onchange = function (e) {
-    render();
-}
+// var orientation_dummy = "default"
+// screen.orientation.onchange = function (e) {
+//     render();
+// }
+
+
+var timeout = false, // holder for timeout id
+    delay = 250, // delay after event is "complete" to run callback
+    calls = 0;
+
+// window.resize event listener
+window.addEventListener('resize', function () {
+    // clear the timeout
+    clearTimeout(timeout);
+    // start timing for event "completion"
+    timeout = setTimeout(render, delay);
+});
+
 
 function checkorientation() {
     calculatezoom();
     var height = parseInt(getBrowserSize().height);
     var width = parseInt(getBrowserSize().width);
-    if (height > width) {
-        document.getElementById("left-side").style.flexDirection = "row";
-        for (var element of document.getElementsByClassName("scorelink")) {
-            element.classList.remove("horizontal-orientation");
-            element.classList.add("vertical-orientation");
-            orientation_dummy = "vertical";
-        }
-    } else {
-        document.getElementById("left-side").style.flexDirection = "column";
-        for (var element of document.getElementsByClassName("scorelink")) {
-            element.classList.add("horizontal-orientation");
-            element.classList.remove("vertical-orientation");
-            orientation_dummy = "horizontal";
-        }
-    }
+    // if (height > width) {
+    //     document.getElementById("left-side").style.flexDirection = "row";
+    //     for (var element of document.getElementsByClassName("scorelink")) {
+    //         element.classList.remove("horizontal-orientation");
+    //         element.classList.add("vertical-orientation");
+    //         orientation_dummy = "vertical";
+    //     }
+    // } else {
+    //     document.getElementById("left-side").style.flexDirection = "column";
+    //     for (var element of document.getElementsByClassName("scorelink")) {
+    //         element.classList.add("horizontal-orientation");
+    //         element.classList.remove("vertical-orientation");
+    //         orientation_dummy = "horizontal";
+    //     }
+    // }
 }
 
 function calculatezoom() {
@@ -68,7 +82,6 @@ function render() {
 
 function displaypdf(pdf_path) {
     pdfjsLib.getDocument(pdf_path).then((pdf) => {
-        checkScreensize();
         myState.pdf = pdf;
         myState.currentPage = 1;
         render();
@@ -80,7 +93,6 @@ function previous() {
         if (myState.pdf == null || myState.currentPage == 1)
             return;
         myState.currentPage -= 1;
-        // document.getElementById("current_page").value = myState.currentPage;
         render();
     });
 }
@@ -89,7 +101,6 @@ function next() {
         if (myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages)
             return;
         myState.currentPage += 1;
-        // document.getElementById("current_page").value = myState.currentPage;
         render();
     });
 }
@@ -113,29 +124,32 @@ function getBrowserSize() {
 }
 
 function checkScreensize() {
-    if (parseInt(getBrowserSize().height) > 1290 && parseInt(getBrowserSize().width) <= 989 && parseInt(getBrowserSize().width) >= 700) {
-        // document.getElementsByClassName("scorelink currentlyActive")[0].style.border = "solid 3px rgba(168, 168, 168, 0.5)";
+    if (parseInt(getBrowserSize().height) > 700 && parseInt(getBrowserSize().width) <= 1000 && parseInt(getBrowserSize().width) > 700) {
+        document.getElementById("scoreview").style.display = "flex";
+        document.getElementById("left-side").style.flexDirection = "row";
+        document.getElementById("profile").style.flexDirection = "row";
         for (var element of document.getElementsByClassName("scorelink")) {
-            element.classList.add("big-screen-vertical");
-            element.classList.remove("big-screen");
             element.classList.remove("small-screen");
+            element.classList.add("vertical-layout");
+            element.classList.remove("horizontal-layout");
         }
+    } else if (parseInt(getBrowserSize().height) > 700 && parseInt(getBrowserSize().width) > 1000) {
         document.getElementById("scoreview").style.display = "flex";
-    } else if (parseInt(getBrowserSize().height) >= 850 && parseInt(getBrowserSize().width) >= 980) {
-        document.getElementById("scoreview").style.display = "flex";
-        // document.getElementsByClassName("scorelink currentlyActive")[0].style.border = "solid 3px rgba(168, 168, 168, 0.5)";
-        for (let element of document.getElementsByClassName("scorelink")) {
-            element.classList.add("big-screen");
-            element.classList.remove("big-screen-vertical");
+        document.getElementById("left-side").style.flexDirection = "column";
+        document.getElementById("profile").style.flexDirection = "row";
+        for (var element of document.getElementsByClassName("scorelink")) {
             element.classList.remove("small-screen");
+            element.classList.add("horizontal-layout");
+            element.classList.remove("vertical-layout");
         }
-        document.getElementById("scoreview").style.display = "flex";
-    } else if (parseInt(getBrowserSize().height) <= 700 || parseInt(getBrowserSize().width) <= 700) {
+    } else if (parseInt(getBrowserSize().height) <= 400 || parseInt(getBrowserSize().width) <= 700) {
         document.getElementById("scoreview").style.display = "none";
+        document.getElementById("left-side").style.flexDirection = "column";
+        document.getElementById("profile").style.flexDirection = "column";
         for (var element of document.getElementsByClassName("scorelink")) {
             element.classList.add("small-screen");
-            element.classList.remove("big-screen-vertical");
-            element.classList.remove("big-screen");
+            element.classList.remove("vertical-layout");
+            element.classList.remove("horizontal-layout");
         }
     }
 }
